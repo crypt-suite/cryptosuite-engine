201d5201aa768149308ed13d2eb4886044a4447f
AK <ak@AKs-MacBook-Air.local>
2026-04-29 19:58:50 +0530

    combined env files

--- backend/server.js
+++ backend/server.js
@@ -44,16 +44,16 @@ const CryptoJS = require("crypto-js");
 
 
 const nodemailer = require("nodemailer");
-
 //2. Production Transporter (Free Gmail Route)
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // Use SSL
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   }
 });
-
 
 
 
